e52a07d3cb26ac95dd139f9549ac60ffa0d2ca6a
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-01-05 18:31:12 -0500

    init: add base project files

---
+++ Blasphemous.Exporting.Maps/Main.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+
+namespace Blasphemous.Exporting.Maps;
+
+[BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
+[BepInDependency("Blasphemous.ModdingAPI", "0.1.0")]
+internal class Main : BaseUnityPlugin
+{
+    public static Maps Maps { get; private set; }
+
+    private void Start()
+    {
+        Maps = new Maps();
+    }
+}
---
+++ Blasphemous.Exporting.Maps/Maps.cs
@@ -0,0 +1,13 @@
+﻿using Blasphemous.ModdingAPI;
+
+namespace Blasphemous.Exporting.Maps;
+
+public class Maps : BlasMod
+{
+    internal Maps() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
+
+    protected override void OnInitialize()
+    {
+        // Perform initialization here
+    }
+}
