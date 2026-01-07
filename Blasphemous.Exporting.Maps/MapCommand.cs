907a4034b5c2c4d2dc73ca302254017b809d8503
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-01-06 19:49:22 -0500

    data: update data through command

--- Blasphemous.Exporting.Maps/MapCommand.cs
+++ Blasphemous.Exporting.Maps/MapCommand.cs
@@ -1,6 +1,8 @@
 ﻿using Blasphemous.CheatConsole;
-using System;
+using Framework.Managers;
 using System.Collections.Generic;
+using System.Linq;
+using Tools.Level.Interactables;
 using UnityEngine;
 
 namespace Blasphemous.Exporting.Maps;
@@ -11,11 +13,16 @@ public class MapCommand : ModCommand
 
     protected override bool AllowUppercase { get; } = false;
 
-    protected override Dictionary<string, Action<string[]>> AddSubCommands()
+    protected override Dictionary<string, System.Action<string[]>> AddSubCommands()
     {
-        return new Dictionary<string, Action<string[]>>()
+        return new Dictionary<string, System.Action<string[]>>()
         {
             { "load", Load },
+            { "door", Door },
+            { "min", Min },
+            { "max", Max },
+
+            // old
             { "setup", Setup },
         };
     }
@@ -53,5 +60,49 @@ public class MapCommand : ModCommand
         }
 
         Main.MapExporter.StartExport(room);
+    }
+
+    private void Door(string[] parameters)
+    {
+        if (!ValidateParameterList(parameters, 0))
+            return;
+
+        string room = Core.LevelManager.currentLevel.LevelName;
+        Vector3 playerPosition = Core.Logic.Penitent.transform.position;
+        Door door = Object.FindObjectsOfType<Door>().OrderBy(x => Vector3.Distance(playerPosition, x.transform.position)).FirstOrDefault();
+
+        if (door == null)
+        {
+            Write("No door could be found");
+            return;
+        }
+
+        Write($"Storing door for {room} as {door.identificativeName}");
+        Main.MapExporter.RoomStorage.UpdateDoor(room, door.identificativeName);
+
+    }
+
+    private void Min(string[] parameters)
+    {
+        if (!ValidateParameterList(parameters, 0))
+            return;
+
+        string room = Core.LevelManager.currentLevel.LevelName;
+        Vector2 position = Camera.main.transform.position;
+
+        Write($"Storing min position for {room} as {position}");
+        Main.MapExporter.RoomStorage.UpdateMinBounds(room, position);
+    }
+
+    private void Max(string[] parameters)
+    {
+        if (!ValidateParameterList(parameters, 0))
+            return;
+
+        string room = Core.LevelManager.currentLevel.LevelName;
+        Vector2 position = Camera.main.transform.position;
+
+        Write($"Storing max position for {room} as {position}");
+        Main.MapExporter.RoomStorage.UpdateMaxBounds(room, position);
     }
 }
--- Blasphemous.Exporting.Maps/RoomStorage.cs
+++ Blasphemous.Exporting.Maps/RoomStorage.cs
@@ -2,6 +2,7 @@
 using Blasphemous.ModdingAPI.Files;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 
 namespace Blasphemous.Exporting.Maps;
 
@@ -29,5 +30,54 @@ public class RoomStorage
     public bool TryGetRoom(string room, out RoomInfo result)
     {
         return _rooms.TryGetValue(room, out result);
+    }
+
+    private RoomInfo CreateRoom(string room)
+    {
+        var info = new RoomInfo()
+        {
+            Name = room,
+            Door = string.Empty,
+        };
+
+        _rooms.Add(room, info);
+        return info;
+    }
+
+    private RoomInfo CreateIfDoesntExist(string room)
+    {
+        if (_rooms.TryGetValue(room, out RoomInfo info))
+            return info;
+
+        return CreateRoom(room);
+    }
+
+    public void UpdateDoor(string room, string door)
+    {
+        RoomInfo info = CreateIfDoesntExist(room);
+
+        info.Door = door;
+
+        // Save
+    }
+
+    public void UpdateMinBounds(string room, Vector2 position)
+    {
+        RoomInfo info = CreateIfDoesntExist(room);
+
+        info.Xmin = position.x;
+        info.Ymin = position.y;
+
+        // Save
+    }
+
+    public void UpdateMaxBounds(string room, Vector2 position)
+    {
+        RoomInfo info = CreateIfDoesntExist(room);
+
+        info.Ymin = position.x;
+        info.Ymax = position.y;
+
+        // Save
     }
 }
