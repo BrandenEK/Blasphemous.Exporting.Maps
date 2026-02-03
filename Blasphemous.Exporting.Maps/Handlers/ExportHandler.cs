41b85f87e6799da5248f82a0b1cdf69bde1f9fe2
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2026-02-02 19:42:30 -0500

    core: clean up export handler

--- Blasphemous.Exporting.Maps/Handlers/ExportHandler.cs
+++ Blasphemous.Exporting.Maps/Handlers/ExportHandler.cs
@@ -21,23 +21,25 @@ public class ExportHandler
 
     public void OnScreenshot(int x, int y)
     {
+        int width = MapExporter.WIDTH;
+        int height = MapExporter.HEIGHT;
+
         _textureHandler.ActivateRenderTexture(true);
 
         Texture2D fullTexture = _textureHandler.ImageTexture;
-        Texture2D partTexture = new Texture2D(WIDTH, HEIGHT, TextureFormat.ARGB32, false);
-        partTexture.ReadPixels(new Rect(0, 0, WIDTH, HEIGHT), 0, 0);
+        Texture2D partTexture = new Texture2D(width, height, TextureFormat.ARGB32, false);
+        partTexture.ReadPixels(new Rect(0, 0, width, height), 0, 0);
         partTexture.Apply();
 
-        Graphics.CopyTexture(partTexture, 0, 0, 0, 0, WIDTH, HEIGHT, fullTexture, 0, 0, x, y);
+        Graphics.CopyTexture(partTexture, 0, 0, 0, 0, width, height, fullTexture, 0, 0, x, y);
 
         byte[] bytes = fullTexture.EncodeToPNG();
-        string folder = Path.Combine(_fileHandler.ContentFolder, $"v{EXPORT_VERSION}");
+        string folder = Path.Combine(_fileHandler.ContentFolder, $"v{MapExporter.EXPORT_VERSION}");
 
         Directory.CreateDirectory(folder);
         File.WriteAllBytes(Path.Combine(folder, $"{Core.LevelManager.currentLevel.LevelName}.png"), bytes);
 
         _textureHandler.ActivateRenderTexture(false);
         Object.Destroy(partTexture);
-
     }
 }
